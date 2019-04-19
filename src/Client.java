d97dfe95d25f18a38f991da0e4a597b908656120
Юлия <Etwaswie@gmail.com>
2019-04-19 22:43:36 +0300

    Laba5

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private static Socket clientSocket; //сокет для общения
+    private static BufferedReader reader;
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            try {
+                clientSocket = new Socket("localhost", 666); // этой строкой мы запрашиваем
+                //  у сервера доступ на соединение
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                while (clientSocket.isConnected()) {
+
+                    System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+
+                    String command = reader.readLine();
+                    out.write(command + "\n");
+                    out.flush();
+                    String serverWord = in.readLine();
+                    System.out.println(serverWord);
+                }
+
+            } finally {
+                System.out.println("Клиент был закрыт...");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+
+    }
+}
+
